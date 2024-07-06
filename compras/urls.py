6a0dd9fc439f9f81f871c01465a4de657a3db68c
Joao Dacoregio <oinaagua6@gmail.com>
2024-07-06 15:35:49 -0300

    navbar pagina de compras

--- compras/urls.py
+++ compras/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
- 
+from .views import compras_home
 urlpatterns = [
-
+    path('',compras_home),
 ]
--- compras/views.py
+++ compras/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def compras_home(request):
+    return render(request,"compras/pages/compras_home.html")  
+
