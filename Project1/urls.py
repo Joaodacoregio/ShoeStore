29e2c2af0d4eac5d880854d6cd6f5371e324ed48
Joao Dacoregio <oinaagua6@gmail.com>
2024-07-06 15:09:57 -0300

    novo app compras

--- Project1/urls.py
+++ Project1/urls.py
@@ -22,5 +22,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("ShoeStore.urls")),
-    path('vendas/',include("vendas.urls"))
+    path('vendas/',include("vendas.urls")),
+    path("compras/",include("compras.urls"))
+
 ]
