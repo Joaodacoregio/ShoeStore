207a18be545c1aee31c9bddd5eb4a5c20d190bb5
Joao Dacoregio <oinaagua6@gmail.com>
2024-07-08 17:10:08 -0300

    teste unitario: populando a pag com dados

--- compras/views.py
+++ compras/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from tests.compras.products.factory import make_product
 
 # Create your views here.
 def compras_home(request):
-    return render(request,"compras/pages/compras_home.html")  
+    return render(request,"compras/pages/compras_home.html" , context={
+        'products':[make_product for _ in range(10)],
+    })  
 
 
 
 def produto(request,id):
-    return render(request,"compras/pages/produto.html")  
+    return render(request,"compras/pages/product-view.html")  
---
+++ tests/compras/products/factory.py
@@ -0,0 +1,25 @@
+from faker import Faker
+from random import randint
+
+
+
+
+fake = Faker("pt_BR")
+
+
+
+def rand_ratio():
+    return randint(840, 900), randint(473, 573)
+
+def make_product(): 
+    return {
+        'title': fake.sentence(nb_words=6),
+        "mark": fake.word(),
+        'gender': fake.random_element(elements=('Masculino', 'Feminino')),
+        'type': fake.word(),
+        'cover': {
+            'url': 'https://loremflickr.com/%s/%s/shoes' % rand_ratio()}
+        }
+ 
+if __name__ =="__main__":
+    print(make_product())
