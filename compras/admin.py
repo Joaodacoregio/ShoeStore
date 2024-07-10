d3bc67a7e8e762591b8bf6ae059ae02bb09966cc
Joao Dacoregio <oinaagua6@gmail.com>
2024-07-10 11:48:47 -0300

    adicionar produto por adm

--- compras/admin.py
+++ compras/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category,Product
 # Register your models here.
 
 
 class CategoryAdm(admin.ModelAdmin):
     ...
 
+
+class ProductAdm(admin.ModelAdmin):
+    ...
+
+admin.site.register(Product,ProductAdm)
 admin.site.register(Category,CategoryAdm)
