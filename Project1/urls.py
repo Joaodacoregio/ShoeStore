3f914d213a6dce7e1ca892d7c87570a11d0efd49
Joao Dacoregio <oinaagua6@gmail.com>
2024-07-10 14:05:40 -0300

    configurando media do django para servir imagens

--- Project1/urls.py
+++ Project1/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
- 
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,3 +26,5 @@ urlpatterns = [
     path('vendas/',include("vendas.urls")),
     path("compras/",include("compras.urls")),
 ]
+
+urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
